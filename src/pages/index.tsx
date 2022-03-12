0c72380511f26bd2f85fb7749115816c5da5c449
unknown <gcolares95@gmail.com>
2022-03-12 17:13:15 -0300

    Componente Subscribe Button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,28 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
-     <Head>
-       <title>Home | Ignews</title>
-     </Head>
+      <Head>
+        <title>Home | Ignews</title>
+      </Head>
 
-     <main className={styles.contentContainer}>
-      <section className={styles.hero}>
-        <span>👏 Hey, welcome</span>
-        <h1>News about the <span>React</span> word.</h1>
-        <p>
-          Get access to all the publications <br />
-          <span>for $9.90 month</span>
-        </p>
-      </section>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> word.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
 
-      <img src="/images/avatar.svg" alt="Girl coding" />     
-    </main> 
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
