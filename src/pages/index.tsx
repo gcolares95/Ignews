f8c3a9dcc0e8b1ecc767e0c3603873f324f7283b
unknown <gcolares95@gmail.com>
2022-03-12 14:42:45 -0300

    Estilos globais

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+     <Head>
+       <title>Ignews | Home</title>
+     </Head>
+     <h1>Home</h1>    
+    </>
+  )
+}
