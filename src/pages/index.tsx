84eaae079aa9b7fb2c0888e594cc66318513a613
unknown <gcolares95@gmail.com>
2022-03-12 15:38:29 -0300

    Página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
      <Head>
-       <title>Ignews | Home</title>
+       <title>Home | Ignews</title>
      </Head>
-     <h1>Home</h1>    
+
+     <main className={styles.contentContainer}>
+      <section className={styles.hero}>
+        <span>👏 Hey, welcome</span>
+        <h1>News about the <span>React</span> word.</h1>
+        <p>
+          Get access to all the publications <br />
+          <span>for $9.90 month</span>
+        </p>
+      </section>
+
+      <img src="/images/avatar.svg" alt="Girl coding" />     
+    </main> 
     </>
   )
 }
