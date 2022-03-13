232f066ef6334c15150ae190681f844c1baa7794
unknown <gcolares95@gmail.com>
2022-03-12 23:16:35 -0300

    Salvando usuário no banco

--- src/components/SignInButton/index.jsx
+++ src/components/SignInButton/index.jsx
@@ -9,19 +9,17 @@ import styles from './styles.module.scss';
 export function SignInButton() {
   const { data: session } = useSession();
 
-  console.log(session);
-
   return session ? (
     <button
       className={styles.signInButton}
       type='button'
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" />
       Guilherme Colares
       <FiX
         color="#737380"
         className={styles.closeIcon}
-        onClick={() => signOut()}
       />
     </button>
   ) : (
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { query as q } from 'faunadb';
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -14,4 +16,23 @@ export default NextAuth({
       }
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      // Salvando no banco após login
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+
+        return true; // retorna true se login deu certo
+      } catch {
+        return false; // evita que o user faça login se nossa app não conseguiu fazer interaçãp c/ banco
+      }
+    },
+  }
 })
