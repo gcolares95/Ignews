8252cb4d52c0197016f5f136300c749fe4b255e3
unknown <gcolares95@gmail.com>
2022-03-17 23:50:36 -0300

    Consumindo API do Prismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,4 +1,8 @@
 import Head from 'next/head';
+import { GetStaticProps } from 'next'
+import { getPrismicClient } from '../../services/prismic';
+import Prismic from '@prismicio/client';
+
 import styles from './styles.module.scss';
 
 export default function Posts() {
@@ -15,18 +19,26 @@ export default function Posts() {
             <strong>Titulo do post em si</strong>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi eius omnis ipsa, animi dignissimos eaque quos non suscipit nemo quod</p>
           </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Titulo do post em si</strong>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi eius omnis ipsa, animi dignissimos eaque quos non suscipit nemo quod</p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Titulo do post em si</strong>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi eius omnis ipsa, animi dignissimos eaque quos non suscipit nemo quod</p>
-          </a>
         </div>
       </main>
     </>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'publication') 
+  ], {
+    fetch: ['publication.title',  'publication.content'], // dados para buscar da publicação
+    pageSize: 100,
+  })
+
+  console.log(JSON.stringify(response, null, 2));
+  //JSON.stringify(obj, null, indentacao)
+
+  return {
+    props: {}
+  }
 }
