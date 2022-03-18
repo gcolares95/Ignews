c71e26822cd5365e1351c0cd9078382349aa9729
unknown <gcolares95@gmail.com>
2022-03-18 01:14:17 -0300

    Navegacão no menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,21 @@
+import Link from 'next/link'
+
 import { SignInButton } from '../SignInButton';
+
 import styles from './styles.module.scss';
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news" />
+         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <Link href="/" >
+            <a className={styles.active}>Home</a>
+          </Link>
+          <Link href="/posts" prefetch>
+           <a>Posts</a>
+          </Link>
         </nav>
 
         <SignInButton />
