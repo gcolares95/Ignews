83e979ca959b8fe2fd8c252d8ba5823acffffdf1
unknown <gcolares95@gmail.com>
2022-03-20 16:38:05 -0300

    pagina preview posts

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -41,7 +41,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
   const { slug } = params;
 
 
-   if (!session.activeSubscription) {
+   if (!session?.activeSubscription) {
      return {
        redirect: {
          destination: '/',
